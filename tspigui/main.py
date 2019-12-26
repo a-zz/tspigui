18a99cf6753878e2e8c645330259e17d42182d47
a-zz <antonio@pareidolia.es>
2019-12-26 11:38:45 +0100

    Code optimization & cleanup.

--- tspigui/main.py
+++ tspigui/main.py
@@ -5,76 +5,82 @@
 # ############################################################################ #
 # TODO Improve error handling for non-existent properties
 # TODO Implement status bar / log widget
-# TODO On-screen keyboard support (matchbox-keyboard)
 from guizero import App, MenuBar
 import os
+import sys
 
-app = None
+_m_ = sys.modules[__name__]
+_m_.app = None
+_m_.r = None
+_m_.kb_subp = None
 
 def init(r):
-    
-    r.log.info('Building GUI')
-    app = App()
+    _m_.r = r
+    _m_.r.log.info('Building GUI')
+    _m_.app = App()
     
     # Window title
-    if r.has_property('gui.title.ro'): 
-        app.title = r.get_property('gui.title.ro')
-        r.log.debug('GUI title set to "%s"', app.title)
+    if _m_.r.has_property('gui.title.ro'): 
+        _m_.app.title = _m_.r.get_property('gui.title.ro')
+        _m_.r.log.debug('GUI title set to "%s"', _m_.app.title)
     else:
-        app.title = 'tspigui'
-        r.log.debug('GUI title set to "pigui" \
+        _m_.app.title = 'tspigui'
+        _m_.r.log.debug('GUI title set to "pigui" \
 (gui.title.ro property not found)')
         
     # Window geometry & appearance
-    geometry = r.get_property('gui.geometry.ro')
+    geometry = _m_.r.get_property('gui.geometry.ro')
     if geometry=='fs':
-        app.tk.attributes("-fullscreen",True)
-        r.log.debug('GUI size set to fullscreen')
+        _m_.app.tk.attributes("-fullscreen", True)
+        _m_.r.log.debug('GUI size set to fullscreen')
     else:
         try:
             w_h = geometry.split('x')
-            app.width = w_h[0]
-            app.height = w_h[1]
-            r.log.debug('GUI size set to %s', geometry)
+            _m_.app.width = w_h[0]
+            _m_.app.height = w_h[1]
+            _m_.r.log.debug('GUI size set to %s', geometry)
         except:
-            r.log.debug('GUI size not set (bad geometry in gui.geometry.ro \
+            _m_.r.log.debug('GUI size not set (bad geometry in gui.geometry.ro \
 property)')
         
-    bg = r.get_property('gui.bg.ro')
-    app.bg = bg
-    r.log.debug('GUI bg colour set to %s', bg)
+    bg = _m_.r.get_property('gui.bg.ro')
+    _m_.app.bg = bg
+    _m_.r.log.debug('GUI bg colour set to %s', bg)
 
     # GUI menu
-    MenuBar(app, ['System'],
+    MenuBar(_m_.app, ['System'],
             [
                 [
-                    ['Exit GUI', lambda:gui_exit(app, r)],
+                    ['Exit GUI', lambda:gui_exit()],
                     ['---', None],
-                    ['Reboot OS', lambda:os_reboot(r)],
-                    ['Halt OS', lambda:os_halt(r)]
+                    ['Reboot OS', lambda:os_reboot()],
+                    ['Halt OS', lambda:os_halt()]
                 ]
             ])
 
     # All done
-    r.log.info('GUI complete, showing now \o/')
-    app.display()
-    r.log.info('GUI closed :_(')
+    _m_.r.log.info('GUI complete, showing now \o/')
+    _m_.app.display()
+    _m_.r.log.info('GUI closed :_(')
     
 # Menu option functions
-def gui_exit(app, r):
-    r.log.debug('Menu option: exit GUI')
-    app.destroy()
 
-def os_reboot(r):
-    r.log.debug('Menu option: reboot OS')
-    exitcode = os.system(r.get_property('gui.cmd.os.reboot.ro'))
-    if exitcode!=0:
-        r.log.error('OS reboot failed, exit code: %d', exitcode)
+def gui_exit():
+    _m_.r.log.debug('Menu option: exit GUI')
+    _m_.app.destroy()
 
-def os_halt(r):
-    r.log.debug('Menu option: halt OS')
-    exitcode = os.system(r.get_property('gui.cmd.os.halt.ro'))
+def os_reboot():
+    cmd = _m_.r.get_property('gui.cmd.os.reboot.ro')
+    _m_.r.log.debug('Menu option: reboot OS (run: %s)', cmd)
+    exitcode = os.system(cmd)
     if exitcode!=0:
-        r.log.error('OS halt failed, exit code: %d', exitcode)
-    
+        _m_.r.log.error('OS reboot failed, exit code: %d', exitcode)
+
+def os_halt():
+    cmd = _m_.r.get_property('gui.cmd.os.halt.ro')
+    _m_.r.log.debug('Menu option: halt OS (run: %s)', cmd)
+    exitcode = os.system(cmd)
+    if exitcode!=0:
+        _m_.r.log.error('OS halt failed, exit code: %d', exitcode)
+
 # ############################################################################ #
